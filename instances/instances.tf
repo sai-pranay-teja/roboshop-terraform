a0c788352037003791cb6e45a7eb07980a48b4aa
saipranayteja <pranayteja.ch@algonox.com>
2023-05-25 15:57:50 +0530

    lkjhg

--- instances/instances.tf
+++ instances/instances.tf
@@ -4,11 +4,11 @@ resource "aws_route53_record" "component-records" {
     name    = "${var.Name}.practise-devops.online"
     type    = "A"
     ttl     = 30
-    records = [aws_spot_instance_request.components.public_ip]
+    records = [aws_instance.components.public_ip]
 }
 
 
-resource "aws_spot_instance_request" "components" {
+/* resource "aws_instance" "components" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = var.instance_type
   vpc_security_group_ids = [ var.security-id ]
@@ -19,9 +19,9 @@ resource "aws_spot_instance_request" "components" {
   tags = {
         Name = var.Name
     }
-}
+} */
 
-/* resource "aws_instance" "components" {
+resource "aws_instance" "components" {
     
     ami           = "ami-0b5a2b5b8f2be4ec2"
     instance_type = var.instance_type
@@ -29,8 +29,8 @@ resource "aws_spot_instance_request" "components" {
 
     tags = {
         Name = var.Name
-    } */
-
+    }
+}
 
     #user_data = "${file("user-data-apache.sh")}"
 
@@ -41,7 +41,7 @@ resource "null_resource" "resource-creation" {
     depends_on = [ aws_route53_record.component-records ]
     provisioner "remote-exec" {
     connection {
-        host = aws_spot_instance_request.components.public_ip
+        host = aws_instance.components.public_ip
         user = "centos"
         password = "DevOps321"
     }
