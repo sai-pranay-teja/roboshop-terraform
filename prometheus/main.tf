d7c46e51e7ce4c670a69ca8657d4b792bbf31b7a
saipranayteja <pranayteja.ch@algonox.com>
2023-06-01 22:48:01 +0530

    sdf

--- prometheus/main.tf
+++ prometheus/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "components" {
+resource "aws_instance" "prometheus" {
     
     ami           = data.aws_ami.centos-ami.id
     instance_type = var.instance_type
@@ -18,15 +18,15 @@ resource "aws_instance" "components" {
 resource "null_resource" "resource-creation" {
     provisioner "remote-exec" {
     connection {
-        host = aws_instance.components.public_ip
-        user = "centos"
-        password = "DevOps321"
+        host = aws_instance.prometheus.public_ip
+        user = data.aws_ssm_parameter.username.value
+        password = data.aws_ssm_parameter.password.value
     }
     inline=[
 
         "sudo labauto ansible",
         "sudo labauto prometheus-server",
-        "sudo ansible-pull -i localhost, -U https://github.com/sai-pranay-teja/roboshop-ansible.git roboshop-app.yml -e component=prometheus"
+        "sudo ansible-pull -i localhost, -U https://github.com/sai-pranay-teja/roboshop-ansible.git roboshop-app.yml -e component=${var.Name}"
     ]  
 
 
