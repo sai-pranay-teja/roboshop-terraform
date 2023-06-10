7442839aae0d0843c20ae4bc5d5ea195adebecde
saipranayteja <pranayteja.ch@algonox.com>
2023-06-10 13:27:52 +0530

    ;lkjhg

--- vars.tf
+++ vars.tf
@@ -2,8 +2,7 @@ variable "zone_id" {}
 variable "env" {}
 variable "vpc"{}
 variable "default_vpc_id" {}
-variable "public_cidr" {}
-variable "private_cidr" {}
+
 
 /* variable "components" {}
 variable "prometheus" {}
