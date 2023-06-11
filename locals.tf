8a5ee50078a4f7f61a494bad839121329f52e773
saipranayteja <pranayteja.ch@algonox.com>
2023-06-11 19:19:58 +0530

    asdfg

--- locals.tf
+++ locals.tf
@@ -12,8 +12,10 @@ locals {
     web=tolist([module.module-vpc["main"].private_subnets["web-az1"].cidr_block, module.module-vpc["main"].private_subnets["web-az2"].cidr_block])
     public=tolist([module.module-vpc["main"].public_subnets["public-az1"].cidr_block, module.module-vpc["main"].public_subnets["public-az2"].cidr_block])
   }
-}
+
 
 db_subnet_ids=tolist([module.module-vpc["main"].private_subnets["db-az1"].id, module.module-vpc["main"].private_subnets["db-az2"].id])
 app_subnet_ids=tolist([module.module-vpc["main"].private_subnets["app-az1"].id, module.module-vpc["main"].private_subnets["app-az2"].id])
 web_subnet_ids=tolist([module.module-vpc["main"].private_subnets["web-az1"].id, module.module-vpc["main"].private_subnets["web-az2"].id])
+
+}
