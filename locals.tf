931013ef2d812a1b30b08d2ddb03be2bd0e5886e
saipranayteja <pranayteja.ch@algonox.com>
2023-06-11 19:33:59 +0530

    sdfg

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   subnet_ids={
-    db=tolist([module.module.module-vpc["main"].private_subnets["db-az1"].id, module.module-vpc["main"].private_subnets["db-az2"].id])
+    db=tolist([module.module-vpc["main"].private_subnets["db-az1"].id, module.module-vpc["main"].private_subnets["db-az2"].id])
     app=tolist([module.module-vpc["main"].private_subnets["app-az1"].id, module.module-vpc["main"].private_subnets["app-az2"].id])
     web=tolist([module.module-vpc["main"].private_subnets["web-az1"].id, module.module-vpc["main"].private_subnets["web-az2"].id])
     public=tolist([module.module-vpc["main"].public_subnets["public-az1"].id, module.module-vpc["main"].public_subnets["public-az2"].id])
