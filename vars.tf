34fda6b0c6608e7dc980795fc30d9d5f0d08a8a3
saipranayteja <pranayteja.ch@algonox.com>
2023-06-13 21:45:56 +0530

    dfvgb

--- vars.tf
+++ vars.tf
@@ -2,6 +2,7 @@ variable "domain_name" {}
 variable "env" {}
 variable "vpc"{}
 variable "default_vpc_id" {}
+variable "bastion_host" {}
 /* variable "docdb"{} */
 variable "rds"{}
 /* variable "elasticache"{} */
