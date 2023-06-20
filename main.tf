eba988a732e04ab8692c0be2955745fbd956e765
saipranayteja <pranayteja.ch@algonox.com>
2023-06-20 13:13:24 +0530

    sdfgv

--- main.tf
+++ main.tf
@@ -117,8 +117,8 @@ module "module-alb" {
 
 
 module "module-app" {
-    depends_on=[ module.module-vpc, module.module-alb, module.docdb, module.rds, module.elasticache, module.rabbitmq ]
-    #depends_on=[ module.module-vpc, module.module-alb ]
+    #depends_on=[ module.module-vpc, module.module-alb, module.docdb, module.rds, module.elasticache, module.rabbitmq ]
+    depends_on=[ module.module-vpc, module.module-alb ]
     source="git::https://github.com/sai-pranay-teja/module-app.git"
     env=var.env
     for_each = var.app
