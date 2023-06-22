3bc4fc456450b032a15431f987b0d989d8347a07
saipranayteja <pranayteja.ch@algonox.com>
2023-06-22 17:43:39 +0530

    /.,

--- prometheus/userdata.sh
+++ prometheus/userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo labauto ansible
-sudo labauto elk
+sudo labauto prometheus-server
 sudo ansible-pull -i localhost, -U https://github.com/sai-pranay-teja/roboshop-ansible.git roboshop-app.yml -e components=${Name}
