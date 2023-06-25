8210537dd5f981e791fdfec13ee646b40e9dac14
saipranayteja <pranayteja.ch@algonox.com>
2023-06-25 14:52:38 +0530

    asdfvg

--- jenkins/iam.tf
+++ jenkins/iam.tf
@@ -37,12 +37,7 @@ resource "aws_iam_policy" "access-policy" {
 		{
 			"Sid": "VisualEditor0",
 			"Effect": "Allow",
-			"Action": [
-        "ssm:*",
-        "ssm:PutParameter",
-        "ssm:DescribeParameters"
-
-      ]
+			"Action": "*",
 			"Resource": [
 				"arn:aws:ec2:*:581798224530:instance/*",
 				"arn:aws:ssm:us-east-1:581798224530:parameter/*"
