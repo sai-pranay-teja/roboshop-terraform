de14a7d029e6f739bc28e65e85c4ae909112c386
saipranayteja <pranayteja.ch@algonox.com>
2023-06-25 14:36:54 +0530

    sxdcf

--- jenkins/iam.tf
+++ jenkins/iam.tf
@@ -31,20 +31,76 @@ resource "aws_iam_role" "access-role" {
 resource "aws_iam_policy" "access-policy" {
   name        = "${var.env}-policy-jenkins"
   path = "/"
-  policy = jsonencode({
-    
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "VisualEditor1",
-          "Effect": "Allow",
-          "Action": "ssm:*",
-          "Resource": "arn:aws:ec2:us-east-1:581798224530:instance/*"
-        }
+  policy = jsonencode(
+    {
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Sid": "VisualEditor0",
+			"Effect": "Allow",
+			"Action": [
+				"ssm:CancelCommand",
+				"ssm:ListCommands",
+				"ssm:DescribeMaintenanceWindowSchedule",
+				"ssm:DescribeInstancePatches",
+				"ssm:CreateActivation",
+				"ssm:CreateOpsItem",
+				"ssm:GetMaintenanceWindowExecutionTaskInvocation",
+				"ssm:DescribeAutomationExecutions",
+				"ssm:DeleteActivation",
+				"ssm:DescribeMaintenanceWindowExecutionTaskInvocations",
+				"ssm:ListOpsMetadata",
+				"ssm:DescribeParameters",
+				"ssm:ListResourceDataSync",
+				"ssm:ListDocuments",
+				"ssm:DescribeMaintenanceWindowsForTarget",
+				"ssm:ListComplianceItems",
+				"ssm:GetMaintenanceWindowExecutionTask",
+				"ssm:GetMaintenanceWindowExecution",
+				"ssm:ListResourceComplianceSummaries",
+				"ssm:ListOpsItemRelatedItems",
+				"ssm:DescribeOpsItems",
+				"ssm:DescribeMaintenanceWindows",
+				"ssm:ListCommandInvocations",
+				"ssm:DescribePatchGroups",
+				"ssm:PutConfigurePackageResult",
+				"ssm:DescribePatchGroupState",
+				"ssm:CreatePatchBaseline",
+				"ssm:GetManifest",
+				"ssm:DeleteInventory",
+				"ssm:DescribeInstancePatchStates",
+				"ssm:DescribeInstancePatchStatesForPatchGroup",
+				"ssm:RegisterManagedInstance",
+				"ssm:GetInventorySchema",
+				"ssm:CreateMaintenanceWindow",
+				"ssm:DescribeInstanceProperties",
+				"ssm:ListInventoryEntries",
+				"ssm:ListOpsItemEvents",
+				"ssm:GetDeployablePatchSnapshotForInstance",
+				"ssm:DescribeSessions",
+				"ssm:DescribePatchBaselines",
+				"ssm:DescribeInventoryDeletions",
+				"ssm:DescribePatchProperties",
+				"ssm:GetInventory",
+				"ssm:DescribeActivations",
+				"ssm:GetCommandInvocation",
+				"ssm:CreateOpsMetadata",
+				"ssm:ListComplianceSummaries",
+				"ssm:PutInventory",
+				"ssm:DescribeInstanceInformation",
+				"ssm:ListAssociations",
+				"ssm:DescribeAvailablePatches"
+			],
+			"Resource": "arn:aws:ec2:us-east-1:581798224530:instance/*"
+		},
+		{
+			"Sid": "VisualEditor1",
+			"Effect": "Allow",
+			"Action": "ssm:*",
+			"Resource": "arn:aws:ec2:us-east-1:581798224530:instance/*"
+		}
 	]
-
-
-  
-  })
+}
+  )
 }
 
