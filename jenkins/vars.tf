f04a58cba4cf7fc8acb1d31e30990f2887b62f90
saipranayteja <pranayteja.ch@algonox.com>
2023-06-25 15:48:38 +0530

    sdfg

--- jenkins/vars.tf
+++ jenkins/vars.tf
@@ -2,3 +2,9 @@ variable "env" {}
 variable "jenkins"{}
 variable "parameters"{}
 variable "domain_name"{}
+
+variable "s-jobs" {
+    default=[
+        {name="Infra", repo_url="https://github.com/sai-pranay-teja/module-jenkins", filename="Infra-setup"}
+    ] 
+}
