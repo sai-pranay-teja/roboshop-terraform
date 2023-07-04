881d3f694c1822a326f69283b04fddfe8723831c
saipranayteja <pranayteja.ch@algonox.com>
2023-07-04 18:47:56 +0530

    lokij

--- main.tf
+++ main.tf
@@ -170,15 +170,84 @@ module "elk-instance" {
 } */
 
 
-module "eks" {
-  source             = "github.com/r-devops/tf-module-eks"
-  ENV                = var.env
-  PRIVATE_SUBNET_IDS = lookup(local.subnet_ids, "app", null)
-  PUBLIC_SUBNET_IDS  = lookup(local.subnet_ids, "public", null)
-  DESIRED_SIZE       = 1
-  MAX_SIZE           = 1
-  MIN_SIZE           = 1
+resource "aws_eks_cluster" "eks" {
+  name     = "${var.ENV}-eks-cluster"
+  role_arn = aws_iam_role.eks-cluster-role.arn
+  version  = var.versionx
+
+  vpc_config {
+    subnet_ids = var.PRIVATE_SUBNET_IDS
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+  ]
 }
+
+resource "aws_iam_role" "eks-cluster-role" {
+  name = "eks-cluster-${var.ENV}-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster-role.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks-cluster-role.name
+}
+
+resource "aws_iam_role" "eks-node-role" {
+  name = "eks-node-${var.ENV}-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy-attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks-node-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy-attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks-node-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly-attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-node-role.name
+}
+
+
 
 
 
