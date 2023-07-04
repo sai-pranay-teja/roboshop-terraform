d2d723d2f0b5d5ed909463a042fcb5fced72cbe9
saipranayteja <pranayteja.ch@algonox.com>
2023-07-04 20:04:36 +0530

    zsxdcf

--- main.tf
+++ main.tf
@@ -169,8 +169,30 @@ module "elk-instance" {
   
 } */
 
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
 
-module "eks" {
+  aws_region          = "us-east-1"
+  cluster_name        = "minikube"
+  aws_instance_type   = "t3.medium"
+  ssh_public_key      = "~/.ssh/id_rsa.pub"
+  aws_subnet_id       = lookup(local.subnet_ids, "public", null)[0]
+  hosted_zone         = "practise-devops.online"
+  hosted_zone_private = false
+
+  tags = {
+    Name = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
+
+/* module "eks" {
   source             = "github.com/r-devops/tf-module-eks"
   ENV                = var.env
   PRIVATE_SUBNET_IDS = lookup(local.subnet_ids, "app", null)
@@ -178,7 +200,7 @@ module "eks" {
   DESIRED_SIZE       = 1
   MAX_SIZE           = 1
   MIN_SIZE           = 1
-}
+} */
 
 
 
