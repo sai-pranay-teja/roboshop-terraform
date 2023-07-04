23b059c7f7a363caa8f9aa37f1adda41ad0c4958
saipranayteja <pranayteja.ch@algonox.com>
2023-07-04 17:00:00 +0530

    mn

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -51,7 +51,7 @@ resource "null_resource" "resource-creation" {
     inline=[
         "sudo labauto terraform",
         "sudo labauto jenkins",
-        "sudo ssh-keygen"
+        "ssh-keygen"
         #"sudo labauto sonar-scanner",
         #"sudo labauto ansible"
     ]  
